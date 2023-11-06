0a1b54e323070c633beff35b352a4720fec9884c
Micha Ober <git@ober-mail.de>
2023-11-06 19:03:43 +0100

    Fix migration to work with all upgrade/downgrade paths

--- migrations/v308/add_modules.php
+++ migrations/v308/add_modules.php
@@ -2,42 +2,38 @@
 
 namespace mober\discordnotifications\migrations\v308;
 
-class add_modules extends \phpbb\db\migration\container_aware_migration
+class add_modules extends \phpbb\db\migration\migration
 {
 	static public function depends_on()
 	{
 		return ['\mober\discordnotifications\migrations\extension_installation'];
 	}
 
-	public function effectively_installed()
-	{
-		/** @var \phpbb\db\migration\tool\module $module_manager */
-		$module_manager = $this->container->get('migrator.tool.module');
-		return $module_manager->exists('acp', 'ACP_DISCORD_NOTIFICATIONS', 'ACP_DISCORD_NOTIFICATIONS_SETTINGS');
-	}
-
 	public function update_data()
 	{
 		return [
-			['module.remove', [
-				'acp',
-				'ACP_DISCORD_NOTIFICATIONS',
-				[
-					'module_langname' => 'ACP_DISCORD_NOTIFICATIONS_TITLE',
-				],
-			]],
 			['module.add', [
 				'acp',
 				'ACP_DISCORD_NOTIFICATIONS',
 				[
 					'module_basename'	=> '\mober\discordnotifications\acp\discord_notifications_module',
 					'modes'				=> [
-						'settings',
 						'webhooks',
 						'mapping',
 					],
 				],
 			]],
+			['custom', [
+				[$this, 'rename_settings_module']
+			]],
 		];
+	}
+
+	public function rename_settings_module()
+	{
+		$sql = 'UPDATE ' . MODULES_TABLE . "
+			SET module_langname = 'ACP_DISCORD_NOTIFICATIONS_SETTINGS'
+			WHERE module_langname = 'ACP_DISCORD_NOTIFICATIONS_TITLE'";
+		$this->sql_query($sql);
 	}
 }
